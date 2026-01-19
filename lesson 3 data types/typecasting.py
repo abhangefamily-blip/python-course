59e0723ed2504928b45aa959ac8611e2df12df78
abhangefamily-blip <abhange.family@gmail.com>
2026-01-19 20:43:19 +0530

    typecasting

---
+++ lesson 3 data types/typecasting.py
@@ -0,0 +1,11 @@
+name = "anushka"
+age = 13
+is_student = True
+weight = 40.5
+print("Data Type of Name is", type(name))
+print("Data Type of Name is", type(age))
+print("Data Type of Name is", type(is_student))
+print("Data Type of name is", type(weight))
+print("\n After typecasting....")
+age = str(age)
+print("Data Type of Name is", type(age))
